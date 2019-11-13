74cb17622fb27187c948c205d142047548c1919b
Muhammad Shakhawat Hossain Safat <mu.shakhawat.hossain@gmail.com>
2019-11-14 00:04:06 +0700

    Initial Commit

---
+++ src/main/java/com/cfranking/CfrankingApplication.java
@@ -0,0 +1,13 @@
+package com.cfranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfrankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfrankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cfranking/CfrankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfrankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
