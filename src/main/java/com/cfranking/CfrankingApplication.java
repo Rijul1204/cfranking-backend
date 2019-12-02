ae0e1e5f9e540f1f1843e0b9278053ce09eabb79
Rashedul Hasan Rijul <rashedulhasanrijul@gmail.com>
2019-12-03 00:36:01 +0800

    added eureka client

--- src/main/java/com/cfranking/CfrankingApplication.java
+++ src/main/java/com/cfranking/CfrankingApplication.java
@@ -2,8 +2,14 @@ package com.cfranking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties(RedisProperties.class)
 public class CfrankingApplication {
 
     public static void main(String[] args) {
